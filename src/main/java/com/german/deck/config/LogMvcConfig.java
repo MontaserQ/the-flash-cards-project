00b939a9769d45145db30a406acf4b9cf631e6d9
Montaser Qasem <montaserrqasem@gmail.com>
2021-12-30 12:01:27 +0100

    Adding loggin config

---
+++ src/main/java/com/german/deck/config/LogMvcConfig.java
@@ -0,0 +1,20 @@
+package com.german.deck.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LogMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CustomRequestInterceptor customRequestInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(customRequestInterceptor)
+                .addPathPatterns("/*");
+    }
+}
+
